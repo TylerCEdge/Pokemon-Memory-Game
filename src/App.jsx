4a5b832bc81dc94c2eb2a5379b698abcfd43851d
atkmstr <atkmstr@localhost.localdomain>
2019-06-13 17:08:51 -0400

    shuffle works i think

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,33 @@ import Header from './components/header'
 import Card from './components/card'
 import cards from './cards.json'
 
+function shuffle (arr) {
+	for (let i = arr.length - 1; i > 0; i--) {
+		let j = Math.floor(Math.random() * (i + 1));
+		[arr[i], arr[j]] = [arr[j], arr[i]];
+	}
+	return arr;
+}
+console.log(shuffle(cards))
+
 class App extends Component {
+
 	state = {
 		cards,
 		score: 0,
-		hiscore: 0,
+		hiscore: 0
+			
 	}
+
+
+	
 
 	render() {
 		return (
 			<div>
-				<Header score={this.state.score} hiscore={this.state.hiscore}>Clicky Game</Header>
+				<Header score={this.state.score} hiscore={this.state.hiscore}>
+					Clicky Game
+				</Header>
 				<div className='container'>
 					<Wrapper>
 						{this.state.cards.map(card => (
