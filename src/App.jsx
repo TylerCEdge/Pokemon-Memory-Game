f43dbac06af66aefee843b0cd0ec9da77c4a49a4
atkmstr <atkmstr@localhost.localdomain>
2019-06-13 16:38:20 -0400

    game board rendering properly next task is to wire onClick

--- src/App.jsx
+++ src/App.jsx
@@ -5,62 +5,22 @@ import Card from './components/card'
 import cards from './cards.json'
 
 class App extends Component {
-	// Setting state
 	state = {
 		cards,
 		score: 0,
 		hiscore: 0,
 	}
 
-	// If game is over
-	gameOver = () => {
-		if (this.state.score > this.state.hiscore) {
-			this.setState({ hiscore: this.state.score }, function() {
-				console.log(this.state.hiscore)
-			})
-		}
-		this.state.cards.forEach(card => {
-			card.count = 0
-		})
-		alert(`Game Over :( \nscore: ${this.state.score}`)
-		this.setState({ score: 0 })
-		return true
-	}
-
-	clickCount = id => {
-		this.state.cards.find((o, i) => {
-			if (o.id === id) {
-				if (cards[i].count === 0) {
-					cards[i].count = cards[i].count + 1
-					this.setState({ score: this.state.score + 1 }, function() {
-						console.log(this.state.score)
-					})
-					this.state.cards.sort(() => Math.random() - 0.5)
-					return true
-				} else {
-					this.gameOver()
-				}
-			}
-		})
-	}
-
 	render() {
 		return (
 			<div>
-				<Header score={this.state.score} hiscore={this.state.hiscore}>
-					Clicky Game
-				</Header>
-				<div className="container">
-				<Wrapper>
-					{this.state.cards.map(card => (
-						<Card
-							clickCount={this.clickCount}
-							id={card.id}
-							key={card.id}
-							image={card.image}
-						/>
-					))}
-				</Wrapper>
+				<Header score={this.state.score} hiscore={this.state.hiscore}>Clicky Game</Header>
+				<div className='container'>
+					<Wrapper>
+						{this.state.cards.map(card => (
+							<Card id={card.id} key={card.id} image={card.image} />
+						))}
+					</Wrapper>
 				</div>
 			</div>
 		)
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,7 +5,7 @@ const NavBar = props => (
         <div className="header">
             <div className="title">{props.children}</div>
             <div className="scores">
-                Score: {props.score} HiScore: {props.hiscore}
+                | Score: {props.score} | HiScore: {props.hiscore} |
             </div>
         </div>
 )
