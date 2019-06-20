4c6bc850ee11923dc7f2b9ab111c0b66366d9ca1
TylerCEdge <tylercedge@gmail.com>
2019-06-20 02:40:48 -0400

    still trying with deploy

---
+++ build/precache-manifest.fb0cfc0cde078094de3b88d3aa5fecc8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c87e8675e01b56302f0efcd7d3f50a2c",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "778dbf1a071f31a6391d",
+    "url": "/clicky-game/static/css/main.2e1137c8.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "778dbf1a071f31a6391d",
+    "url": "/clicky-game/static/js/main.8b784a6d.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "89727a06bb99fd9d5ae35b0f72f695fc",
+    "url": "/clicky-game/static/media/angrypikachu.89727a06.png"
+  },
+  {
+    "revision": "b1350b4a5ad76cba2635d9730c7be09f",
+    "url": "/clicky-game/static/media/arbok.b1350b4a.png"
+  },
+  {
+    "revision": "283350baa3e48da882014f1a00f494ed",
+    "url": "/clicky-game/static/media/background.283350ba.jpg"
+  },
+  {
+    "revision": "6d37ba9a631b3d34d7525606d0aa188f",
+    "url": "/clicky-game/static/media/blueflying.6d37ba9a.png"
+  },
+  {
+    "revision": "b11c75b7366a657d5f3bf06c1a90058f",
+    "url": "/clicky-game/static/media/blueguy.b11c75b7.png"
+  },
+  {
+    "revision": "e4a98ddcb6c333a158250f9e0b4e9c0d",
+    "url": "/clicky-game/static/media/bowser.e4a98ddc.png"
+  },
+  {
+    "revision": "1772f9f87c8bf9a46ab594eabc610171",
+    "url": "/clicky-game/static/media/greyguy.1772f9f8.png"
+  },
+  {
+    "revision": "703836e87a1d0e28796de9f8a269ec81",
+    "url": "/clicky-game/static/media/greysomething.703836e8.png"
+  },
+  {
+    "revision": "37702ed0a24db4f928d84a37a51a680d",
+    "url": "/clicky-game/static/media/mario.37702ed0.png"
+  },
+  {
+    "revision": "1ea36257e373290475ab7f292f806fb3",
+    "url": "/clicky-game/static/media/naruto.1ea36257.png"
+  },
+  {
+    "revision": "f45ffc8a2b99fc350c80680abd6e5431",
+    "url": "/clicky-game/static/media/peach.f45ffc8a.png"
+  },
+  {
+    "revision": "c7469c870e7b4121d60d8078ffeb7f57",
+    "url": "/clicky-game/static/media/pikachu.c7469c87.png"
+  },
+  {
+    "revision": "70dad4ce4016985897d814df65be6351",
+    "url": "/clicky-game/static/media/yellow.70dad4ce.png"
+  }
+]);
