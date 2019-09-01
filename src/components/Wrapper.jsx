42d8285843e597aec47f6625749eacd5d5a0d40e
TylerCEdge <tylercedge@gmail.com>
2019-08-31 23:14:47 -0400

    making some progress.  Grid layout styled using grid-template-colummns and rows images rendered with react-bootstrap

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container } from 'react-bootstrap'
+import './Styles/styles.css'
 
-const Wrapper = props => <Container>{props.children}</Container>;
+const Wrapper = props => <Container className="test">{props.children}</Container>;
 
 export default Wrapper;
